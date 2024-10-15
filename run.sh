9a8da44d8e25dc73d031d8ebcb262b999e1c42d5
Theo Theunissen <theotheu@Mac.lan>
2024-10-15 20:34:58 +0200

    gitignore update with macos and linux

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update && sudo apt-get install make build-essential libssl-dev \
     zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+    git
 
 curl https://pyenv.run | bash
 
